ce0e8007d08acfff33fe73c0dc2e81ad73bb8659
Alvaro Molina <miguel.molina@nuevatel.com>
2018-04-25 23:54:02 -0400

    prueba 2

--- src/main/java/com/ucbcba/proyecto2018/Proyecto2018Application.java
+++ src/main/java/com/ucbcba/proyecto2018/Proyecto2018Application.java
@@ -2,7 +2,7 @@ package com.ucbcba.proyecto2018;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//Prueba
+//Pruebas ss
 @SpringBootApplication
 public class Proyecto2018Application {
 
