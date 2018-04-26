28b8693742a89c910af41063dbbb29865bbf9e41
Alvaro Molina <miguel.molina@nuevatel.com>
2018-04-25 23:41:57 -0400

    first

---
+++ src/main/java/com/ucbcba/proyecto2018/Proyecto2018Application.java
@@ -0,0 +1,12 @@
+package com.ucbcba.proyecto2018;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto2018Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto2018Application.class, args);
+	}
+}
